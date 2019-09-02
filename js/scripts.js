1b908811292e7ad8e210ad3deba88eea563f2f79
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-09-02 09:12:07 +0300

    Adds pizza constructors to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
-function 
+function Pizza(size, crust, toppings) {
+  this.size = [];
+  this.crust = [];
+  this.toppings = [];
+
+
+}
+function size(small, medium, large) {
+  this.small = small;
+  this.medium = medium;
+  this.large = large;
+}
+function crust(crispy, stuffed, gluttenFree) {
+  this.crispy = crispy;
+  this.stuffed = stuffed;
+  this.gluttenFree = gluttenFree;
+}
+function toppings(pepperoni, mushrooms, bacon, sausage,blackOlives, cheese) {
+  this.pepperoni = pepperoni;
+  this.mushrooms = mushrooms;
+  this.bacon = bacon;
+  this.sausage = sausage;
+  this.blackOlives = blackOlives;
+  this.cheese = cheese;
+}
+
 
 $(document).ready(function(){
+  
     
   });
